ac22d885e0c01c582c08e3cd8edaa9d4a753ea28
YamamotoTakumi <57386527+taku-yamamoto@users.noreply.github.com>
2019-12-25 14:29:37 +0900

    Qiita React-ToDoList Fin

---
+++ src/ToDoListItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './ToDoListItem.css';
+
+class ToDoListItem extends Component {
+  render() {
+    const {
+        title,
+        description,
+        ...props
+      } = this.props;
+
+    return (
+      <div className="ToDoListItem"　{...props}>
+        <div className="ToDoListItem-title">{title}</div>
+        <div className="ToDoListItem-description">{description}</div>
+      </div>
+    );
+  }
+}
+
+export default ToDoListItem;
